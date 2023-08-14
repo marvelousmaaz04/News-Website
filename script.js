de43dcebe72c1cc486d5fe91c9173a5e42cfc034
marvelousmaaz04 <133691098+marvelousmaaz04@users.noreply.github.com>
2023-08-14 23:14:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = "ff213539420c4385b30dccc52205cc88"
-const url = "https://newsapi.org/v2/everything?q="
+const url = "http://newsapi.org/v2/everything?q="
 
 window.addEventListener("load", () => {
     fetchNews("India");
